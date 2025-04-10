023b3cb878e831efd094dcb0c86ae02e6f9c1f7c
Jose Morais <devjosemorais@gmail.com>
2025-04-10 16:19:13 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { app } from "./server/servidor";
+
+app.listen(3000, ()=>{
+    console.log("Servidor está rodando!")
+})
---
+++ src/server/servidor.ts
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Jesus Cristo")
+})
+
+interface Teste {
+    
+}
+
+
+
+export {app}
